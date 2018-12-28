304204681103b9b21cad054fd4163bd9dde842a8
Ray Hancock <ray.hancock@aprimo.com>
2018-12-28 12:02:00 -0500

    added background and movement to player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+	}
+}
