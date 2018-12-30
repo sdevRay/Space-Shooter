a05b6f88ab5fd985535480b1f318339da0209208
Ray Hancock <ray.hancock@aprimo.com>
2018-12-30 02:13:09 -0500

    asteroid destroys

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,9 +2,15 @@
 
 public class DestroyByContact : MonoBehaviour {
 
+	// Use this for initialization
 	private void OnTriggerEnter(Collider other)
 	{
+		if(other.tag == "Boundary")
+		{
+			return;
+		}
 		Destroy(other.gameObject);
 		Destroy(gameObject);
 	}
+
 }
