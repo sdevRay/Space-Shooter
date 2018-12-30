dd4ef454e2652a04f151f5724a8adce5ad324303
Ray Hancock <ray.hancock@aprimo.com>
2018-12-30 01:07:29 -0500

    add in asteroid

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+	private Rigidbody rb;
+	public float tumble;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
